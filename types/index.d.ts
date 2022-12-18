9be520d67355fb3c3033437008095086130b780e
Alex Aguirre <teamaguirre@gmail.com>
2022-12-18 13:57:28 -0800

    created the function definitions for loaddash

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,8 @@
-declare module 'lodash' {}
+declare module 'lodash' {
+// the multiply method
+    function multiply(num1:number,num2:number):number
+// generic type reverse function
+    function reverse<Type>(arr:Type[]):Type[]
+// Drop right function
+    function dropRight (arr:(string |number)[],dropped:number):(string | numnber)[]
+}
